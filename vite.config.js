f1ae6db0c210c798f8b2ff0c4fb8c78a22e6afe6
jayasukhwani <157370200+jayasukhwani@users.noreply.github.com>
2024-09-27 11:41:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/website/"
+  base: '/website/',  // This must match your repository name
 })
